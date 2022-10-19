193d658f16256e6b2d0ed10acc3364115c1837bf
Wenchy <wenchy.zwz@gmail.com>
2022-10-19 23:08:47 +0800

    fix(xml): use explicit header settings to avoid implicit settings exception

--- internal/importer/xml.go
+++ internal/importer/xml.go
@@ -160,7 +160,7 @@ func readXMLFile(metasheet, content string, newBook *book.Book) (*tableaupb.XMLB
 			return nil, errors.Wrapf(err, "failed to parseMetaNode for sheet:%s", sheetName)
 		}
 	}
-	
+
 	// strip template sheets
 	for sheet, colMap := range metasheetMap {
 		if template, ok := colMap["Template"]; !ok || template != "true" {
@@ -211,7 +211,11 @@ func genMetasheet(tableauNode *xmlquery.Node) (map[string]map[string]string, *bo
 			sheetMap[attr.Name.Local] = attr.Value
 		}
 		sheetMap["Nested"] = "true"
-		// param in `config.yaml` may not be one
+		// use explicit settings to avoid implicit settings exception.
+		sheetMap["Namerow"] = "1"
+		sheetMap["Typerow"] = "2"
+		sheetMap["Noterow"] = "3"
+		sheetMap["Datarow"] = "4"
 		sheetMap["Nameline"] = "1"
 		sheetMap["Typeline"] = "1"
 		sheetName, ok := sheetMap["Sheet"]
