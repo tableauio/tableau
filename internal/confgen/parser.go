3fd90eebc62438802a132629995e7484f6fefd61
Wenchy <wenchy.zwz@gmail.com>
2023-07-21 21:14:08 +0800

    feat: add ecode: list elements are not present continuously

--- internal/confgen/parser.go
+++ internal/confgen/parser.go
@@ -833,7 +833,7 @@ func (sp *sheetParser) parseListField(field *Field, msg protoreflect.Message, rc
 			// squeeze to specified fixed size
 			size = fixedSize
 		}
-		checkRemainFlag := false
+		var firstNonePresentIndex int
 		for i := 1; i <= size; i++ {
 			newListValue := list.NewElement()
 			colName := prefix + field.opts.Name + strconv.Itoa(i)
@@ -885,17 +885,17 @@ func (sp *sheetParser) parseListField(field *Field, msg protoreflect.Message, rc
 						}
 					}
 				}
-				if checkRemainFlag {
+				if firstNonePresentIndex != 0 {
 					// Check that no empty element is existed in between, so we should guarantee
 					// that all the remaining elements are not present, otherwise report error!
 					if elemPresent {
 						kvs := append(rc.CellDebugKV(colName), xerrors.KeyPBFieldType, "horizontal struct list")
-						return false, xerrors.ErrorKV("elements are not present continuously", kvs...)
+						return false, xerrors.WithMessageKV(xerrors.E2016(firstNonePresentIndex, i), kvs...)
 					}
 					continue
 				}
 				if !elemPresent && !prop.IsFixed(field.opts.Prop) {
-					checkRemainFlag = true
+					firstNonePresentIndex = i
 					continue
 				}
 				list.Append(newListValue)
@@ -911,16 +911,16 @@ func (sp *sheetParser) parseListField(field *Field, msg protoreflect.Message, rc
 					kvs := append(rc.CellDebugKV(colName), xerrors.KeyPBFieldType, "horizontal scalar list")
 					return false, xerrors.WithMessageKV(err, kvs...)
 				}
-				if checkRemainFlag {
-					// check the remaining keys all not present, otherwise report error!
+				if firstNonePresentIndex != 0 {
+					// check the remaining scalar elements are not present, otherwise report error!
 					if elemPresent {
 						kvs := append(rc.CellDebugKV(colName), xerrors.KeyPBFieldType, "horizontal scalar list")
-						return false, xerrors.ErrorKV("elements are not present continuously", kvs...)
+						return false, xerrors.WithMessageKV(xerrors.E2016(firstNonePresentIndex, i), kvs...)
 					}
 					continue
 				}
 				if !elemPresent && !prop.IsFixed(field.opts.Prop) {
-					checkRemainFlag = true
+					firstNonePresentIndex = i
 					continue
 				}
 				list.Append(newListValue)
