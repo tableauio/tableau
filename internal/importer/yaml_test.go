e7bf16f54de0fc114532a028d6d7b74877eb58e9
wenchy <wenchy.zwz@gmail.com>
2024-08-09 16:02:51 +0800

    protogen(yaml): only read schema documents for templating data document (#122)
    * protogen(yaml): only read schema documents for templating data document
    
    * fix: unittest coverage

--- internal/importer/yaml_test.go
+++ internal/importer/yaml_test.go
@@ -1,13 +1,12 @@
 package importer
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"testing"
 
-	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
 	"github.com/tableauio/tableau/internal/importer/book"
+	"google.golang.org/protobuf/proto"
 	"gopkg.in/yaml.v3"
 )
 
@@ -16,58 +15,58 @@ func Test_inspectYAMLNode(t *testing.T) {
 	data := []byte(`
 ---
 "@sheet": "@TABLEAU"
-LiteConf:
-LoaderConf:
-  OrderedMap: true
+ServiceConf:
+  Template: true
 ---
-ScalarList: [1, 2, 3]
-Servers:
-  gamesvr:
-    Name: gamesvr
-    Confs:
-      ItemConf:
-        Async: true
-      DropConf:
-        Async: true
-  mailsvr:
-    Name: mailsvr
-    Confs:
-      ItemConf:
-        Async: true
-      DropConf:
-        Async: true
+# define schema
+"@sheet": "@ServiceConf"
+ID: uint32
+Name: string
+---
+"@sheet": ServiceConf
+ID: {{ env.id }}
+Name: {{ env.name}}
+{% if env.name == 'prod' %}
+Enabled: true
+{% else %}
+Enabled: false
+{% endif %}
 `)
 
-	// Create a new decoder
-	dec := yaml.NewDecoder(bytes.NewReader(data))
-	for i := 0; ; i++ {
+	rawDocs, err := extractRawYAMLDocuments(string(data))
+	require.NoError(t, err)
+	for i, rawDoc := range rawDocs {
+		if !isSchemaSheet(rawDoc) {
+			continue
+		}
 		var node yaml.Node
-		// Decode one document at a time
-		err := dec.Decode(&node)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			} else {
-				t.Fatalf("error: %v", err)
-			}
-		}
+		err := yaml.Unmarshal([]byte(rawDoc), &node)
+		require.NoError(t, err)
 		sheet, err := parseYAMLSheet(&node, i)
-		if err != nil {
-			t.Fatalf("%+v", err)
-		}
+		require.NoError(t, err)
 		fmt.Println(sheet.String())
 	}
 }
 
-func Test_readYAMLBook(t *testing.T) {
+type TestSheetParser struct {
+}
+
+func (p *TestSheetParser) Parse(protomsg proto.Message, sheet *book.Sheet) error {
+	return nil
+}
+
+func TestNewYAMLImporter(t *testing.T) {
 	type args struct {
-		filename string
-		parser   book.SheetParser
+		filename   string
+		sheetNames []string
+		parser     book.SheetParser
+		mode       ImporterMode
+		cloned     bool
 	}
 	tests := []struct {
 		name    string
 		args    args
-		want    *book.Book
+		want    *YAMLImporter
 		wantErr bool
 	}{
 		{
@@ -77,17 +76,25 @@ func Test_readYAMLBook(t *testing.T) {
 				parser:   nil,
 			},
 		},
+		{
+			name: "TestTemplate.yaml",
+			args: args{
+				filename: "testdata/TestTemplate.yaml",
+				mode:     Protogen,
+				parser:   &TestSheetParser{},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := readYAMLBook(tt.args.filename, tt.args.parser)
+			got, err := NewYAMLImporter(tt.args.filename, tt.args.sheetNames, tt.args.parser, tt.args.mode, tt.args.cloned)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("readYAMLBook() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("NewYAMLImporter() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			fmt.Println(got.String())
 			// if !reflect.DeepEqual(got, tt.want) {
-			// 	t.Errorf("readYAMLBook() = %v, want %v", got, tt.want)
+			// 	t.Errorf("NewYAMLImporter() = %v, want %v", got, tt.want)
 			// }
 		})
 	}
