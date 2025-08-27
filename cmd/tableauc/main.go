b3669c319585b11473f75e7a7f8a4d5d701761a6
wenchy <wenchy.zwz@gmail.com>
2025-08-27 15:38:51 +0800

    release: tableauc v0.14.2

--- cmd/tableauc/main.go
+++ cmd/tableauc/main.go
@@ -15,7 +15,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const version = "0.14.1"
+const version = "0.14.2"
 const (
 	ModeDefault = "default" // generate both proto and conf files
 	ModeProto   = "proto"   // generate proto files only
