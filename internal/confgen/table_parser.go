8120de072e321f82b1cf4e56b494f91cdcb0eaef
Wenchy <wenchy.zwz@gmail.com>
2025-10-17 14:05:36 +0800

    fieldprop(optional): support list and map fields for table sheet (#309)
    * feat: support list field optional
    
    * feat: support optional map field
    
    * feat: add test for struct, scalar, and wellknown fields
    
    * feat: renaming field prop sheet
    
    * feat: rearrange field prop sheets and add FieldPropRefer

--- internal/confgen/table_parser.go
+++ internal/confgen/table_parser.go
@@ -277,7 +277,11 @@ func (sp *tableParser) parseHorizontalMapField(field *Field, msg protoreflect.Me
 	newPrefix := prefix + field.opts.Name
 	detectedSize := rc.GetCellCountWithPrefix(newPrefix)
 	if detectedSize <= 0 {
-		return false, xerrors.Errorf("no cell found with digit suffix")
+		if sp.IsFieldOptional(field) {
+			// map field not found and is optional, just return false, nil.
+			return false, nil
+		}
+		return false, xerrors.Errorf("no cell found with digit suffix for horizontal map field")
 	}
 	fixedSize := fieldprop.GetSize(field.opts.Prop, detectedSize)
 	size := detectedSize
@@ -493,7 +497,11 @@ func (sp *tableParser) parseHorizontalListField(field *Field, msg protoreflect.M
 	newPrefix := prefix + field.opts.Name
 	detectedSize := rc.GetCellCountWithPrefix(newPrefix)
 	if detectedSize <= 0 {
-		return false, xerrors.Errorf("no cell found with digit suffix")
+		if sp.IsFieldOptional(field) {
+			// list field not found and is optional, just return false, nil.
+			return false, nil
+		}
+		return false, xerrors.Errorf("no cell found with digit suffix for horizontal list field")
 	}
 	fixedSize := fieldprop.GetSize(field.opts.Prop, detectedSize)
 	size := detectedSize
